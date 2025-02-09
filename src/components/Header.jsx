209f27cf734e3e6f431130d2435d7073a8acd2aa
lacriman <proskurin.yarik@gmail.com>
2025-02-09 19:50:32 +0100

    feat: add dynamic category icons that change on selection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import './Header.css';
-import headerImg from '../assets/header_logo.png';
 
 function Header() {
    return (
-      <img src={headerImg} alt="" className="header-img" />
+      <img src='/assets/header_logo.png' alt="" className="header-img" />
    );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import faqData from "../data/faqData";
 import './Sidebar.css';
 
 function Sidebar({ onSelectCategory }) {
+   const [activeCategory, setActiveCategory] = useState(faqData[0]?.menu);
+
+   const handleCategoryClick = (category) => {
+      setActiveCategory(category);
+      onSelectCategory(category);
+   };
+
    return (
       <div className="sidebar">
          <ul className="sidebar-list">
             {faqData.map((category) => (
                <li
                   key={category.menu}
-                  className="sidebar-item"
-                  onClick={() => onSelectCategory(category.menu)}
+                  className={`sidebar-item ${activeCategory === category.menu ? 'active' : ''}`}
+                  onClick={() => handleCategoryClick(category.menu)}
                >
-                  <img src="../assets/bullet.png" alt="" />
-                  <a href="#" className="sidebar-link">{category.menu}</a>
+                  <img
+                     src={activeCategory === category.menu ? '/assets/bullet_over.png' : '/assets/bullet.png'}
+                     alt="bullet"
+                     className="sidebar-bullet"
+                  />
+                  <a href="#" className="sidebar-link">
+                     {category.menu}
+                  </a>
                </li>
             ))}
          </ul>
