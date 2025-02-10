af833c782d9686d34a4f7bda447a902cc64af857
lacriman <proskurin.yarik@gmail.com>
2025-02-10 21:01:08 +0100

    feat: implement search input with state management and trigger functionality
    - Added controlled search input with state management
    - Implemented search button click handler
    - Enabled search trigger on 'Enter' key press

--- src/components/FaqSearch.jsx
+++ src/components/FaqSearch.jsx
@@ -3,12 +3,35 @@ import faqData from "../data/faqData";
 import './FaqSearch.css';
 
 function FaqSearch() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => {
+    // event.target.value gives you the current value of the input field.
+    // setSearchTerm() updates the state with the new value.
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log("Search Term:", searchTerm);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="search-container">
-      <h2 className="search-title">Suche:</h2>
+      <label htmlFor="search-input" className="search-title">Suche:</label>
       <div className="search-content">
-        <input type="text" id="search-input" />
-        <button id="search-button">
+        <input
+          type="text"
+          id="search-input"
+          value={searchTerm}
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+        />
+        <button onClick={handleSearch} id="search-button">
           <img src="/assets/btn_suche.png" alt="" />
         </button>
       </div>
